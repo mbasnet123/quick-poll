5f2514260c3588dda501d16cf1139afa727bfbff
mbasnet123 <mbasnet868@gmail.com>
2026-01-05 23:52:42 +0545

    refactor: modify poll_entity to use freezed

--- lib/features/create_poll/domain/poll_entity.dart
+++ lib/features/create_poll/domain/poll_entity.dart
@@ -1,18 +1,27 @@
 import 'package:quick_poll/shared/enums/poll_type.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'poll_entity.freezed.dart';
+part 'poll_entity.g.dart';
 
-class PollEntity {
-  final String id;
-  final String title;
-  final String? description;
-  final String? creatorId;
-  final String creatorName;
-  final DateTime createdAt;
-  final DateTime? expiresAt;
-  final PollType pollType;
-  final bool isAnonymous;
-  final bool allowMultipleVotes;
-  final PollStatus status;
-  final List<PollQuestion> questions;
-  final int totalVotes;
-  final List<String> tags;
+@freezed
+class PollEntity with _$PollEntity {
+  const factory PollEntity({
+    required String id,
+    required String title,
+    String? description,
+    required String creatorId,
+    required String creatorName,
+    required DateTime createdAt,
+    DateTime? expiresAt,
+    required PollType pollType,
+    @Default(false) bool isAnonymous,
+    @Default(false) bool allowMultipleVotes,
+    required PollStatus status,
+    required List<PollQuestion> questions,
+    @Default(0) int totalVotes,
+    @Default([]) List<String> tags,
+  }) = _PollEntity;
+
+  factory PollEntity.fromJson(Map<String, dynamic> json) =>
+      _$PollEntityFromJson(json);
 }
