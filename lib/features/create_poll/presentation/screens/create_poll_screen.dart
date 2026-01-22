ae66595326e0824664c69028904e4ef5f47eba6b
mbasnet123 <mbasnet868@gmail.com>
2026-01-22 21:04:15 +0545

    feat: display poll question

--- lib/features/create_poll/presentation/screens/create_poll_screen.dart
+++ lib/features/create_poll/presentation/screens/create_poll_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quick_poll/core/utils/validation_util.dart';
 
 class CreatePollScreen extends StatefulWidget {
   const CreatePollScreen({super.key});
@@ -8,36 +9,78 @@ class CreatePollScreen extends StatefulWidget {
 }
 
 class _CreatePollScreenState extends State<CreatePollScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final questionController = TextEditingController();
+  String pollQuestion = "";
+
+  @override
+  void dispose(){
+    questionController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    final _formKey = GlobalKey<FormState>();
-    final questionController = TextEditingController();
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    controller: questionController,
-                    decoration: const InputDecoration(
-                      labelText: "Poll Question",
-                      hintText: "What would you like to ask?",
-                      border: OutlineInputBorder(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+          child: Column(
+            children: [
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Text("Poll Question"),
+                    TextFormField(
+                      controller: questionController,
+                      decoration: InputDecoration(
+                        hintText: "What would you like to ask?",
+                        filled: true,
+                        fillColor: Colors.yellowAccent,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16),
+                          borderSide:BorderSide.none,
+                        ),
+                      ),
+                      validator: ValidationUtil.pollQuestionValidator,
+                      onSaved: (value) {
+                        pollQuestion = value ?? "";
+                      },
                     ),
-                    validator: (value){
-                      if (value == null || value.isEmpty) {
-                        return "Question is required";
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+                    SizedBox(height: 10,),
+                    GestureDetector(
+                      onTap: (){
+                        final isValidated = _formKey.currentState?.validate() ?? false;
+                        if(!isValidated) return;
+                        _formKey.currentState!.save();
+                        setState(() {
+                        });
+                        questionController.clear();
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.greenAccent,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text("Submit"),
+                      ),
+                    ),
+                    if (pollQuestion.isNotEmpty)
+                      SizedBox(height: 10,),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.greenAccent,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(pollQuestion),
+                      )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
