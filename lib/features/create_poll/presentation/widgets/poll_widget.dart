e9fea2058211ccb5496fa061f660aac8617bdcb1
mbasnet123 <mbasnet868@gmail.com>
2026-03-27 23:56:35 +0545

    feat(wip): pass poll_data to pollWidget and display in home_screen

--- lib/features/create_poll/presentation/widgets/poll_widget.dart
+++ lib/features/create_poll/presentation/widgets/poll_widget.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
 class PollWidget extends StatelessWidget {
-  const PollWidget({super.key});
+  final String question;
+  final String? description;
+  final String pollType;
+  final IconData pollTypeIcon;
+
+  const PollWidget({
+    super.key,
+    required this.question,
+    this.description,
+    required this.pollType,
+    required this.pollTypeIcon,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         color: Color(0xffE8E5E5FF),
-        borderRadius: BorderRadius.circular(32)
+        borderRadius: BorderRadius.circular(32),
       ),
       child: Column(
-
+        children: [
+          Text(question),
+          Text(description ?? "no desc"),
+        ],
       ),
     );
   }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:quick_poll/features/create_poll/presentation/widgets/poll_widget.dart';
 
 import 'core/app/config/route/paths.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,16 +20,24 @@ class HomeScreen extends StatelessWidget {
           children: [
             Text("Start your poll"),
             SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () {
-                context.go(Paths.createPollScreenRoute.path);
-              },
-              child: Text("Poll Screen"),
-            ),
-
+            SizedBox(height: 8,),
+            PollWidget(question: question, pollType: pollType, pollTypeIcon: pollTypeIcon),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: () async{
+        final result = await context.push(Paths.createPollScreenRoute.path);
+
+        if (result != null && result is Map<String, dynamic>){
+          setState((){
+            question = result['question'] as String?;
+            description = result['description'] as String?;
+            pollType = result['pollType'] as String?;
+            pollTypeIcon = result['pollTypeIcon'] as IconData?;
+          });
+        }
+      },
+      child: Text("Poll Screen"),),
     );
   }
 }
