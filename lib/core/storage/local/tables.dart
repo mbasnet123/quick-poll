8e455688f28dcc4a0d1e60d29b07d91068cafa84
mbasnet123 <mbasnet868@gmail.com>
2026-05-05 23:59:06 +0545

    feat: add PollOptions Table

--- lib/core/storage/local/tables.dart
+++ lib/core/storage/local/tables.dart
@@ -8,3 +8,14 @@ class Poll extends Table{
   @override
   Set<Column> get primaryKey => {id};
 }
+
+class PollOptions extends Table{
+  TextColumn get id => text()();
+  TextColumn get pollId => text().references(Polls, #id, onDelete: KeyAction.cascade)();
+  TextColumn get optionText => text()();
+  IntColumn get order => integer().nullable()();
+  IntColumn get voteCount => integer().withDefault(const Constant(0))();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
