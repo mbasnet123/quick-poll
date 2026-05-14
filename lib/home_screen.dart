e6fda802446a6ee9ac86e000456b257af6ec52cb
mbasnet123 <mbasnet868@gmail.com>
2026-05-14 23:57:43 +0545

    fix: update home_screen with Riverpod

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:quick_poll/core/providers/poll_list_notifier.dart';
 import 'package:quick_poll/features/create_poll/data/model/poll_model.dart';
 import 'package:quick_poll/features/create_poll/presentation/widgets/poll_widget.dart';
 
 import 'core/app/config/route/paths.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends ConsumerState<HomeScreen> {
   String? question;
   String? description;
   String? pollType;
@@ -21,47 +23,42 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final pollsAsync = ref.watch(pollListNotifierProvider);
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            polls.isEmpty
-                ? Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.poll_outlined, size: 64, color: Colors.grey),
-                        SizedBox(height: 16),
-                        Text("No polls yet", style: TextStyle(fontSize: 18)),
-                        Text(
-                          "Tap + to create one",
-                          style: TextStyle(color: Colors.grey),
-                        ),
-                      ],
+        child: pollsAsync.when(
+          data: (polls) {
+            if (polls.isEmpty) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.poll_outlined, size: 64, color: Colors.grey),
+                    SizedBox(height: 16),
+                    Text("No polls yet", style: TextStyle(fontSize: 18)),
+                    Text(
+                      "Tap + to create one",
+                      style: TextStyle(color: Colors.grey),
                     ),
-                  )
-                : Expanded(
-                  child: ListView.builder(padding: EdgeInsets.all(16), itemCount: polls.length, itemBuilder: (context, index) {
-                    final poll = polls[index];
-                    return PollWidget(poll: poll);
-                              }),
+                  ],
                 ),
-          ],
+              );
+            }
+            return ListView.builder(
+              padding: const EdgeInsets.all(16),
+              itemCount: polls.length,
+              itemBuilder: (context, index) {
+                final poll = polls[index];
+                return PollWidget(poll: poll);
+              },
+            );
+          },
+          error: (err, stack) => Center(child: Text('Error: $err')),
+          loading: () => const Center(child: CircularProgressIndicator()),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final result = await context.push(Paths.createPollScreenRoute.path);
-
-          if (result != null && result is Map<String, dynamic>) {
-            setState(() {
-              question = result['question'] as String?;
-              description = result['description'] as String?;
-              pollType = result['pollType'] as String?;
-              pollTypeIcon = result['pollTypeIcon'] as IconData?;
-            });
-          }
-        },
+        onPressed: () => context.push(Paths.createPollScreenRoute.path),
         child: Icon(Icons.add),
       ),
     );
